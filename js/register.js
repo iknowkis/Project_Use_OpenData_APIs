dff129f58bb268df1111439b61a45cfa1f832d06
김인수 <kis877@naver.com>
2022-09-08 00:42:30 +0900

    Init 🎉

---
+++ js/register.js
@@ -0,0 +1,6 @@
+let loginData = document.querySelectorAll(".form-control");
+let RegButton = document.querySelector("#regist");
+RegButton.addEventListener("click", clickRegist);
+function clickRegist() {
+    
+}
